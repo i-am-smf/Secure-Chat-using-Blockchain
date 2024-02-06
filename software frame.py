7ab8cfaf8ebb66c398685f65f7f9c26ace91ca0d
SHAIK MOHAMED FAHAD <100957377+i-am-smf@users.noreply.github.com>
2024-02-06 16:47:13 +0530

    icon updated

--- software frame.py
+++ software frame.py
@@ -2,7 +2,9 @@ from tkinter import *
 import datetime
 
 main=Tk()
+main.title("Z Chat")
 main.geometry("1200x800")
+main.iconbitmap("icon.ico")
 main.resizable(False,False)
 
 info_frame = Frame(main,background="#AFEEEE",width=200)
@@ -13,12 +15,12 @@ chatframe.pack(side=RIGHT, fill=BOTH, expand=True)
 
 for i in range(1,11):
     if i%2==0:
-        labelframe=LabelFrame(chatframe,text=i)
+        labelframe=LabelFrame(chatframe,text=datetime.datetime.now().__format__("%d-%m-%Y %H:%M:%S"))
         labelframe.pack(padx=10,pady=10,anchor=E)
     else:
-        labelframe=LabelFrame(chatframe,text=i)
+        labelframe=LabelFrame(chatframe,text=datetime.datetime.now().__format__("%d-%m-%Y %H:%M:%S"))
         labelframe.pack(padx=10,pady=10,anchor=W)
 
-    Label(labelframe,text=datetime.datetime.now()).pack(anchor=E)
+    Label(labelframe,text=i).pack(anchor=E)
 
 main.mainloop()
